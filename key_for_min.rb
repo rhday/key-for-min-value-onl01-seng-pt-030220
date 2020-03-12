c69e66bb2f6852db49c065c0c1baebf2e372d650
Ruari Day <ruariday@gmail.com>
2020-03-12 07:41:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
+  lowest_key = nil
+  lowest_value = 0 
+  name_hash.each do |key, value|
+    if lowest value == 0 || value < lowest_value
+      lowest_value = value 
+      lowest_key = key
+    end
+  end 
+  lowest_key
 end
