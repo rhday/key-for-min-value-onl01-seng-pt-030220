c7c65b7a06e81ff35062d2e871b1f82c6b6f072f
Ruari Day <ruariday@gmail.com>
2020-03-12 07:52:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
-def key_for_min_value(name_hash)
+def key_for_min_value(name_hashhash)
   lowest_key = nil
   lowest_value = Float::INFINITY
-  name_hashhash.each do |key, value|
+  name_hash.each do |key, value|
     if value < lowest_value
       lowest_value = value
       lowest_key = key
